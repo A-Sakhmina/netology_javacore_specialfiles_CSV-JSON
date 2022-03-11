4c50e00d673602ed3f8c4ce73bc92365a5d2c236
Sakhmina <asadullaevasakhmina@gmail.com>
2022-03-11 23:05:33 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,69 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //массив строчек, содержащий информацию о предназначении колонок в CVS файле:
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+
+        //опредеение имени для считываемого CSV файла:
+        String fileName = "data.csv";
+
+        //получение списка сотрудников с помощью parseCSV()
+        List<Employee> list = parseCSV(columnMapping, fileName);
+
+        String json = listToJson(list);
+        String jsonFileName = "data.json";
+        writeString(json, jsonFileName);
+
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            // определяется класс, к которому будут привязаны данные из CSV документа,
+            // а также порядок расположения полей в этом документе
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            //CsvToBean создает инструмент для взаимодействия CSV документа и выбранной ранее стратегии
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader).withMappingStrategy(strategy).build();
+            // распарсить CSV файл в список объектов
+            List<Employee> staff = csv.parse();
+            staff.forEach(System.out::println);
+            return staff;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        String json = gson.toJson(list, listType);
+
+        return json;
+    }
+
+    public static void writeString(String json, String jsonFileName) {
+        try (FileWriter file = new FileWriter(jsonFileName)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
